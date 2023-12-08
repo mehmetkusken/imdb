5b7d07298281e73cb234d9472e5f4a1bb8bde463
Mehmet <mehmetkusken@gmail.com>
2023-12-08 15:47:28 +0300

    navbar is ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
+ //= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
